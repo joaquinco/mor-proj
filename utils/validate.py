c7f3d0e45de3c536f47683fa9b8564968a4a7dba
Joaquín Correa <joaquincorrea333@gmail.com>
2020-11-09 21:55:30 -0300

    add time validation to validate.py

--- utils/validate.py
+++ utils/validate.py
@@ -7,16 +7,29 @@ from misc import log_err
 def validate_solution(data):
   instance = data.get('instance')
   distances = instance.get('distances')
+  clients = {c.get('id'): c for c in instance.get('clients')}
   vehicles = {v.get('id'): v for v in instance.get('vehicles')}
   solution = data.get('solution')
   routes = solution.get('routes')
 
+  allowed_deviation = instance.get('allowed_deviation')
+
   val = 0
   for route in routes:
-    clients = route.get('clients')
+    route_clients = route.get('clients')
     val += vehicles.get(route.get('vehicle_id')).get('fixed_cost')
-    for c1, c2 in zip(clients[1:], clients[:-1]):
-      val += distances[c1.get('client_id')][c2.get('client_id')]
+    current_time = 0
+    for c1, c2 in zip(route_clients[1:], route_clients[:-1]):
+      client2 = clients.get(c2.get('client_id'))
+      arc_time = distances[c1.get('client_id')][c2.get('client_id')]
+      current_time = max(client2.get('earliest'), arc_time + current_time)
+      allowed_offset = (client2.get('latest') - client2.get('earliest')) * allowed_deviation
+      client_latest = client2.get('latest') + allowed_deviation
+
+      assert current_time <= client_latest, f"Client {client2.get('id')} arrival time is {current_time} but latest is {client_latest}"
+
+      current_time += client2.get('service_time')
+      val += arc_time
 
   solution_val = solution.get('value')
   assert val == solution_val, f"Expected solution value of {val}, found {solution_val}"
